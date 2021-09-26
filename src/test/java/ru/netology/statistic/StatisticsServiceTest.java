a5368d44555da3bb49caedd49a55dac60235b11e
Dovion <vyacheslav-98@yandex.ru>
2021-09-27 02:06:32 +0300

    Failure check

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,8 +10,10 @@ class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 12, 11, 11};
         long expected = 12;
+
+
 
         long actual = service.findMax(incomesInBillions);
 
